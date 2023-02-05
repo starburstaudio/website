e1ce4a7085722a819593b784b97f46f7dfa9a5f9
stxarixdust <sylveon_ari@hotmail.com>
2023-02-06 00:28:05 +0100

    Make the product grid responsive

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -137,31 +137,37 @@ class PoductsPage extends React.Component {
                         </div>
                     </IconContext.Provider>
                     </div>
-                    <div className="flex-row flex flex-wrap justify-between gap-8 pb-8"><IconContext.Provider value={{ size: "2em" }}>
-                        {this.state.results.map(r => (
-                            <div key={r.productId} className="card w-80 bg-base-100 card-bordered indicator">
-                                <figure><img src={r.productAsset.preview} /></figure>
-                                <div className="card-body p-6">
-                                    <h2 className="card-title">
-                                        {r.productName}
+                    <div
+                      className="gap-8 pb-8"
+                      style={{
+                        display: "grid",
+                        gridTemplateColumns: "repeat(auto-fill, minmax(16rem, 1fr))"
+                      }}
+                    ><IconContext.Provider value={{ size: "2em" }}>
+                    {this.state.results.map(r => (
+                        <div key={r.productId} className="card w-auto bg-base-100 card-bordered indicator">
+                            <figure><img src={r.productAsset.preview} /></figure>
+                            <div className="card-body p-6">
+                                <h2 className="card-title">
+                                    {r.productName}
 
-                                        {this.formatBadge(r.facetValueIds[0])} 
-                                    </h2>
-                                    <div
-                                      dangerouslySetInnerHTML={{__html: r.description}}
-                                      className="pb-4 text-md max-h-36 overflow-hidden"
-                                      style={{maskImage: "linear-gradient(to top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1) 50%)"}}
-                                    />
-                                    <div className="card-actions justify-end items-center">
-                                        <Link className="btn btn-ghost text-md btn-sm" to={"/p/" + r.slug}>More Info</Link>
-                                        <div className="btn btn-primary">
-                                            <span className="mr-2 text-base">{this.formatPrice(r.priceWithTax)}</span>
-                                            <FiPlusCircle/>
-                                        </div>
+                                    {this.formatBadge(r.facetValueIds[0])} 
+                                </h2>
+                                <div
+                                  dangerouslySetInnerHTML={{__html: r.description}}
+                                  className="pb-4 text-md max-h-36 overflow-hidden"
+                                  style={{maskImage: "linear-gradient(to top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1) 50%)"}}
+                                />
+                                <div className="card-actions justify-end items-center">
+                                    <Link className="btn btn-ghost text-md btn-sm" to={"/p/" + r.slug}>More Info</Link>
+                                    <div className="btn btn-primary">
+                                        <span className="mr-2 text-base">{this.formatPrice(r.priceWithTax)}</span>
+                                        <FiPlusCircle/>
                                     </div>
                                 </div>
                             </div>
-                        ))}
+                        </div>
+                    ))}
                     </IconContext.Provider></div>
                 </div>
            </main>
