7894ff7b89bdbe6ca39538d4b7aed2eee48543d2
luna <sylveon_ari@hotmail.com>
2023-02-27 15:41:37 +0100

    Allow `Sign Up` / `Log In` switching

--- client/src/common/components/AccountCard.jsx
+++ client/src/common/components/AccountCard.jsx
@@ -1,27 +1,28 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { IconContext } from 'react-icons'
-import { FiEye, FiInfo } from 'react-icons/fi'
+import { FiEye, FiEyeOff } from 'react-icons/fi'
 
 class AccountCard extends React.Component {
-  init = () => {}
+  constructor(props) {
+    super(props)
+    this.state = {
+      signUpMode: false
+    }
+  }
+
+  switchMode() {
+    this.setState({ signUpMode: !this.state.signUpMode })
+  }
 
   render() {
     return (
       <div className="card shadow bg-base-200 grow border-base-300 border max-w-md m-auto">
         <div className="card-body space-y-2">
-          <h1 className="text-3xl text-center">Login</h1>
+          <h1 className="text-3xl text-center">
+            {this.state.signUpMode ? 'Sign Up' : 'Log In'}
+          </h1>
           <p className="opacity-75 text-center">
-            Log in with your e-mail and password
-            <div
-              className="tooltip inline"
-              data-tip="You need to be logged into an account. This is because you will get a download link via email, and on your personal account page.">
-              <button className="btn btn-circle cursor-default no-animation btn-xs btn-ghost ml-2 align-middle translate-y-[-.2rem]">
-                <IconContext.Provider value={{ size: '1.25rem' }}>
-                  <FiInfo></FiInfo>
-                </IconContext.Provider>
-              </button>
-            </div>
+            {this.state.signUpMode ? 'Sign Up' : 'Log In'} with{' '}
+            {this.state.signUpMode ? 'an' : 'your'} e-mail adress and password
           </p>
           <div className="form-control w-full">
             <label className="label">
@@ -32,28 +33,43 @@ class AccountCard extends React.Component {
           <div className="form-control w-full">
             <label className="label">
               <span className="label-text">Password</span>
-              <Link
-                className="link-primary label-text-alt"
-                tabIndex="-1"
-                to="/">
-                Forgot Password?
-              </Link>
+              <span className="label-text-alt">
+                {!this.state.signUpMode && (
+                  <a
+                    className="link-primary"
+                    tabIndex="-1"
+                    href="/password-reset">
+                    Forgot Password?
+                  </a>
+                )}
+              </span>
             </label>
             <div className="input-group">
               <input type="password" className="input input-bordered w-full" />
-              <div className="btn btn-square no-animation">
-                <FiEye />
-              </div>
+              <label className="swap">
+                <input type="checkbox" />
+                <div className="btn rounded-l-none no-animation swap-on">
+                  <FiEyeOff />
+                </div>
+                <div className="btn rounded-l-none no-animation swap-off">
+                  <FiEye />
+                </div>
+              </label>
             </div>
           </div>
           <div className="form-control">
             <label className="label cursor-pointer">
               <span className="label-text">Remember me</span>
-              <input type="checkbox" className="checkbox" />
+              <input type="checkbox" className="checkbox checkbox-primary" />
             </label>
           </div>
           <button className="btn btn-primary">Log in</button>
-          <button className="btn">Create Account</button>
+          <div>
+            <span className="opacity-75">Don&apos;t have an account yet? </span>
+            <a className="link link-primary" onClick={() => this.switchMode()}>
+              {this.state.signUpMode ? 'Log In' : 'Sign Up'}
+            </a>
+          </div>
         </div>
       </div>
     )
--- client/src/pages/checkout/CheckoutPage.jsx
+++ client/src/pages/checkout/CheckoutPage.jsx
@@ -16,7 +16,6 @@ class CheckoutPage extends React.Component {
 
   getCurrentOrder() {
     this.state.order.getCurrentOrder().then((o) => {
-      console.log(o)
       this.setState({ order: o })
     })
   }
@@ -28,14 +27,14 @@ class CheckoutPage extends React.Component {
   render() {
     return (
       <main className="flex flex-col items-center justify-between">
-        <div className="all-width py-24">
+        <div className="all-width pt-24">
           <div className="flex border-b border-base-300 py-6">
             <div className="h-min">
               <h1 className="text-4xl">Checkout</h1>
             </div>
           </div>
           <div className="flex gap-x-8">
-            <div className="grow pr-8 border-r border-base-300">
+            <div className="grow pr-8 pb-8">
               <div className="w-full my-6">
                 <ul className="steps w-full">
                   <li className="step step-primary">Account Log-In</li>
@@ -45,36 +44,40 @@ class CheckoutPage extends React.Component {
               </div>
               <AccountCard />
             </div>
-            <div className="w-80">
-              <h2 className="text-3xl my-6">Your Order</h2>
+            <div className="w-96">
+              <h2 className="text-2xl my-6">Your Order</h2>
               {this.state.order.lines.products.map((l) => (
-                <div className="flex-row flex mb-4 items-center" key={l.ID}>
-                  <img
-                    className="h-20 w-20 p-0 rounded-xl border-base-300 border"
-                    src={l.featuredAsset.preview}
-                  />
+                <div className="flex-row flex mb-6 items-center" key={l.ID}>
+                  <div>
+                    <img
+                      className="h-24 w-24 p-0 rounded-lg saturate-150 brightness-150 opacity-20 blur-xl absolute"
+                      src={l.featuredAsset.preview}
+                    />
+                    <img
+                      className="h-24 w-24 p-0 rounded-lg relative"
+                      src={l.featuredAsset.preview}
+                    />
+                  </div>
                   <div className="grow ml-6 cursor-default">
-                    <h3 className="text-base leading-tight">{l.name}</h3>
-                    <p className="text-sm opacity-75">{l.formatPrice()}</p>
+                    <h3 className="text-lg font-bold leading-tight mb-2">
+                      {l.name}
+                    </h3>
+                    <p className="text-base opacity-75">{l.formatPrice()}</p>
                   </div>
                   <div
                     className="btn btn-ghost text-primary btn-circle color-primary btn-sm "
                     onClick={() => this.removeFromOrder(l.ID)}>
-                    <IconContext.Provider value={{ size: '1.25rem' }}>
+                    <IconContext.Provider value={{ size: '1.5rem' }}>
                       <FiMinusCircle></FiMinusCircle>
                     </IconContext.Provider>
                   </div>
                 </div>
               ))}
-              <div className="py-3 border-t border-base-300">
+              <div className="py-6 border-t border-base-300">
                 <div className="opacity-75 text-right">Total (after VAT)</div>
                 <div className="text-xl text-right font-bold">
                   {Product.formatPrice(this.state.order.totalWithTax)}
                 </div>
-              </div>
-              <div className="flex items-center gap-4">
-                <div className="btn btn-ghost">Back</div>
-                <div className="btn btn-primary grow">Continue</div>
               </div>
             </div>
           </div>
