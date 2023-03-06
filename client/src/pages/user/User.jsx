22e6d6f803472f57054e191b518e5968b6a46dc5
luna <sylveon_ari@hotmail.com>
2023-03-06 07:55:24 +0100

    Highlight hovered line

--- client/src/pages/user/User.jsx
+++ client/src/pages/user/User.jsx
@@ -55,9 +55,9 @@ class UserProducts extends React.Component {
           </div>
           <div className="w-full grow">
             <h2 className="text-3xl my-6">Your products</h2>
-            <div className="space-y-4">
+            <div className="space-y-0">
               <div
-                className="grid gap-y-4 font-bold text-lg"
+                className="grid gap-y-4 font-bold"
                 style={{
                   'grid-template-columns': '1fr 1fr 1fr auto'
                 }}>
@@ -74,7 +74,7 @@ class UserProducts extends React.Component {
               </div>
               {[0, 0, 0, 0, 0].map((v) => (
                 <div
-                  className="grid gap-y-4"
+                  className="grid gap-y-4 hover:bg-base-200 box-border border-transparent hover:border-base-300 border p-3 -mx-3 rounded-xl transition group"
                   key={v}
                   style={{
                     'grid-template-columns': '1fr 1fr 1fr auto'
@@ -84,7 +84,7 @@ class UserProducts extends React.Component {
                       <div className="w-16 h-16">
                         <img
                           src="http://localhost:3001/assets/preview/33/frame-2x1024__preview.png"
-                          className="w-16 h-16 rounded-lg saturate-150 brightness-150 opacity-glow blur-md absolute"
+                          className="w-16 h-16 rounded-lg saturate-150 brightness-150 opacity-0 transition group-hover:opacity-75 blur-md absolute"
                         />
                         <img
                           src="http://localhost:3001/assets/preview/33/frame-2x1024__preview.png"
@@ -93,24 +93,30 @@ class UserProducts extends React.Component {
                       </div>
                       <div>
                         <div className="font-bold">Heatburst</div>
-                        <div className="text-sm opacity-50">Plugin</div>
+                        <div className="text-sm opacity-50 group-hover:opacity-100 transition">
+                          Plugin
+                        </div>
                       </div>
                     </div>
                   </td>
-                  <td className="content-center flex items-center">
+                  <td className="content-center flex items-center opacity-75 group-hover:opacity-100 transition">
                     <div>17th of January 2023</div>
                   </td>
-                  <td className="content-center flex items-center font-mono">
+                  <td className="content-center flex items-center font-mono opacity-75 group-hover:opacity-100 transition">
                     <div>SERI-AL12-345-678-9XY-Z</div>
                   </td>
                   <IconContext.Provider value={{ size: '1.5rem' }}>
-                    <td className="content-center flex items-center">
-                      <a className="btn btn-ghost btn-circle text-primary">
-                        <FiDownload></FiDownload>
-                      </a>
-                      <a className="btn btn-ghost btn-circle text-primary">
-                        <FiLink></FiLink>
-                      </a>
+                    <td className="content-center flex items-center opacity-0 group-hover:opacity-100 transition">
+                      <div className="tooltip" data-tip="Download">
+                        <a className="btn btn-ghost btn-circle text-primary">
+                          <FiDownload></FiDownload>
+                        </a>
+                      </div>
+                      <div className="tooltip" data-tip="Open store page">
+                        <a className="btn btn-ghost btn-circle text-primary">
+                          <FiLink></FiLink>
+                        </a>
+                      </div>
                     </td>
                   </IconContext.Provider>
                 </div>
